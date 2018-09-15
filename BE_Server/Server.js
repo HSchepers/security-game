b7fad8744967c7189efcd655bc72bb286cb665a3
HSchepers <henning.schepers@hs-osnabrueck.de>
2018-09-15 15:36:28 +0200

    SQL interaction

--- BE_Server/Server.js
+++ BE_Server/Server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const content = require('./custom_modules/content_types');
 const fs = require('fs');
+const mysql = require('mysql');
 
 var app = express();
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var URLencodedParser = bodyParser.urlencoded({ extended: false })
 
+//--MIDDLEWARE--------------------------------------------------------------
 //This piece of middleware will log the requests
-app.use('/', function(req, res, next){
+app.use('/', function (req, res, next) {
   console.log('Request: ' + req.url);
   next(); //next piece of middleware
 });
@@ -17,12 +19,13 @@ app.use('/', function(req, res, next){
 //serve the requested file
 app.use('/assets', express.static('assets'));
 
-app.get('/', function(req, res){
+//--GET-REQUESTS------------------------------------------------------------
+app.get('/', function (req, res) {
   res.writeHead(200, content.plain);
   res.end('This is the Back-End-Server');
 });
 
-app.get('/json', function(req, res){
+app.get('/jsontest', function (req, res) {
   var myObject = {
     name: 'Henning',
     age: 21,
@@ -34,12 +37,42 @@ app.get('/json', function(req, res){
 });
 
 //Dynamic Routing for everything that doesn't use one of the above
-app.get('/:file', function(req, res){
-    res.writeHead(200, content.html);
-    var myReadStream = fs.createReadStream(__dirname + '/404.html', 'utf8');
-    myReadStream.pipe(res);
+app.get('/:file', function (req, res) {
+  res.writeHead(200, content.html);
+  var myReadStream = fs.createReadStream(__dirname + '/404.html', 'utf8');
+  myReadStream.pipe(res);
+});
+
+//--POST-REQUESTS-----------------------------------------------------------
+app.post('/login', URLencodedParser, function (req, res) {
+  console.log(req.body);
+
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "guest",
+    password: "login",
+    database: "securitygame"
+  });
+
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to Database");
+
+    //var sql = 'select count(user) from users where user = "HSchepers" and auth_string = password("Password123")';
+    var sql = 'select * from securitygame.scores';
+
+    var result = connection.query(sql, function (err, rows, fields) {
+      if (err) throw err;
+
+      console.log(rows);
+
+      return rows;
+    });
+    connection.end();
+  });
+  //res.writeHead(200, content.json);
+  
+  res.end();
 });
 
 app.listen(3000);
-
-//test
